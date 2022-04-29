c5fa24b9fc4ff1f2bc9f6390a66806ded3dd04d0
ItokianaRa <rajohnsonitokiana@gmail.com>
2022-04-29 23:02:51 +0300

    Create matieres.js

---
+++ model/matieres.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let MatiereSchema = Schema({
+    id: Number,
+    matiere: String,
+    photo: String
+});
+
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('matieres', MatiereSchema);
